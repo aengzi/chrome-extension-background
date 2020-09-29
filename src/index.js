8825a107c258f6c66f308e6b9729ebd116137e6c
jongwoo Yoo <dbwhddn10@gmail.com>
2020-09-30 02:21:46 +0900

    add prettier and setup

--- src/index.js
+++ src/index.js
@@ -19,81 +19,93 @@ chrome.browserAction.onClicked.addListener(() => {
   });
 });
 
-chrome.webRequest.onHeadersReceived.addListener((details) => {
-  const baseUrl = chrome.extension.getURL('').substr(0, chrome.extension.getURL('').length - 1);
-  const headers = details.responseHeaders;
-  const initUrl = details.initiator;
-  const { url } = details;
-  const isReqUrl = initUrl.match(new RegExp(`^${baseUrl}`));
-  const isWantUrl = url.match(new RegExp('^http://101.79.136.27/'))
-    || url.match(new RegExp('^http://101.79.136.36/'))
-    || url.match(new RegExp('^http://211.110.86.208/'))
-    || url.match(new RegExp('^http://vod-archive-kr-cdn-z01.afreecatv.com/'))
-    || url.match(new RegExp('^http://.+api.aengzi.com/'))
-    || url.match(new RegExp('^https://storage.googleapis.com/aengzi.com/'));
+chrome.webRequest.onHeadersReceived.addListener(
+  (details) => {
+    const baseUrl = chrome.extension
+      .getURL('')
+      .substr(0, chrome.extension.getURL('').length - 1);
+    const headers = details.responseHeaders;
+    const initUrl = details.initiator;
+    const { url } = details;
+    const isReqUrl = initUrl.match(new RegExp(`^${baseUrl}`));
+    const isWantUrl =
+      url.match(new RegExp('^http://101.79.136.27/')) ||
+      url.match(new RegExp('^http://101.79.136.36/')) ||
+      url.match(new RegExp('^http://211.110.86.208/')) ||
+      url.match(new RegExp('^http://vod-archive-kr-cdn-z01.afreecatv.com/')) ||
+      url.match(new RegExp('^http://.+api.aengzi.com/')) ||
+      url.match(new RegExp('^https://storage.googleapis.com/aengzi.com/'));
 
-  if (isReqUrl && isWantUrl) {
-    headers.push({
-      name: 'Access-Control-Allow-Origin',
-      value: '*',
-    });
-  }
+    if (isReqUrl && isWantUrl) {
+      headers.push({
+        name: 'Access-Control-Allow-Origin',
+        value: '*',
+      });
+    }
 
-  return {
-    responseHeaders: headers,
-  };
-}, {
-  urls: [
-    'http://*.aengzi.com/',
-    'http://101.79.136.27/*',
-    'http://101.79.136.36/*',
-    'http://211.110.86.208/*',
-    'http://vod-archive-kr-cdn-z01.afreecatv.com/*',
-    'https://storage.googleapis.com/aengzi.com/*',
-  ],
-}, [
-  'blocking',
-  'responseHeaders',
-  'extraHeaders',
-]);
+    return {
+      responseHeaders: headers,
+    };
+  },
+  {
+    urls: [
+      'http://*.aengzi.com/',
+      'http://101.79.136.27/*',
+      'http://101.79.136.36/*',
+      'http://211.110.86.208/*',
+      'http://vod-archive-kr-cdn-z01.afreecatv.com/*',
+      'https://storage.googleapis.com/aengzi.com/*',
+    ],
+  },
+  ['blocking', 'responseHeaders', 'extraHeaders']
+);
 
 setInterval(() => {
   const notifiedAt = moment.utc().format('YYYY-MM-DD HH:00:00');
 
-  if (moment.utc().format('mm') !== '00' || localStorage.getItem('notified_at') === notifiedAt) {
+  if (
+    moment.utc().format('mm') !== '00' ||
+    localStorage.getItem('notified_at') === notifiedAt
+  ) {
     return;
   }
 
   /* eslint-disable no-underscore-dangle */
   chrome.instanceID.getID((instanceId) => {
-    axios.post(`${apiUrl}/devices`, {
-      related_id: instanceId,
-      related_type: 'chrome',
-    }).then((obj) => obj.data.result).then((device) => {
-      axios.patch(`${apiUrl}/devices/${device._attributes.id}`, {
+    axios
+      .post(`${apiUrl}/devices`, {
         related_id: instanceId,
         related_type: 'chrome',
-      });
-      axios.get(`${apiUrl}/notifications`, {
-        params: {
-          after: localStorage.getItem('notified_at') ? localStorage.getItem('notified_at') : notifiedAt,
-        },
-      }).then((obj) => obj.data.result.data).then((notifications) => {
-        localStorage.setItem('notified_at', notifiedAt);
-        notifications.forEach((notification) => {
-          chrome.notifications.create(null, {
-            type: 'basic',
-            iconUrl: './main/assets/img/aengzi_icon_origin.jpg',
-            title: notification._attributes.type,
-            message: notification._attributes.description,
-            buttons: [
-              { title: '확인' },
-            ],
-            priority: 0,
-          });
+      })
+      .then((obj) => obj.data.result)
+      .then((device) => {
+        axios.patch(`${apiUrl}/devices/${device._attributes.id}`, {
+          related_id: instanceId,
+          related_type: 'chrome',
         });
+        axios
+          .get(`${apiUrl}/notifications`, {
+            params: {
+              after: localStorage.getItem('notified_at')
+                ? localStorage.getItem('notified_at')
+                : notifiedAt,
+            },
+          })
+          .then((obj) => obj.data.result.data)
+          .then((notifications) => {
+            localStorage.setItem('notified_at', notifiedAt);
+            notifications.forEach((notification) => {
+              chrome.notifications.create(null, {
+                type: 'basic',
+                iconUrl: './main/assets/img/aengzi_icon_origin.jpg',
+                title: notification._attributes.type,
+                message: notification._attributes.description,
+                buttons: [{ title: '확인' }],
+                priority: 0,
+              });
+            });
+          });
       });
-    });
   });
 }, 60 * 1000);
 
@@ -102,9 +114,7 @@ chrome.notifications.create('init', {
   iconUrl: './main/assets/img/aengzi_icon_origin.jpg',
   title: '설치 성공',
   message: 'BJ 앵지 확장프로그램을 설치해주셔서 감사합니다.',
-  buttons: [
-    { title: '확인' },
-  ],
+  buttons: [{ title: '확인' }],
   priority: 0,
 });
 
